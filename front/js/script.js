8f032e4e28bf2e676a63ff3240043ad3d0360412
Tanguy Dorval <dorvaltanguy@gmail.com>
2022-11-16 15:22:07 +0100

    Affichage du premier canapé (bug)

--- front/js/script.js
+++ front/js/script.js
@@ -1,23 +1,50 @@
+/*Fonction qui appelle l'API */
 function apiCall() {
   fetch('http://localhost:3000/api/products')
     .then(reponse => reponse.json())
     .then(reponse => displayProducts(reponse))
-      document.querySelector(".productName").innerHTML = products.name;
-      document.querySelector(".colors").innerHTML = products.colors;
-      document.querySelector(".description").innerHTML = products.description;
-      document.querySelector(".imageUrl").innerHTML = products.imageUrl;
-      document.querySelector(".altTxt").innerHTML = products.name;
-      document.querySelector(".price").innerHTML = products.price;
-      document.querySelector("._id").innerHTML = products._id;
+      
 }
 
+
+/*Fonction affiche les canapés */
 function displayProducts(reponseApi) {
+  
+  /*Séléction de l'ID items*/
+  const productSelection = document.getElementById("items");
+  
   const products = reponseApi;
-  console.log(products);
-  for (let product of products) {
-        console.log(product);
-    }
-}
+  console.table(products);
+  for (let i = 0; i < displayProducts.length; i++) {
+    
+      /*Insértion de l'élèment a*/
+      const a = document.createElement('a');
+      document.querySelector(".items").appendChild(a);
+      a.href = `product.html?id=${products[i]._id}`;
+      
 
-apiCall();
- 
+      /*Insértion de l'élèment article*/
+      const productArticle = document.createElement("article");
+      a.appendChild(productArticle);
+      
+      /*Insértion de l'image + alt*/
+      const productImg = document.createElement("img");
+      productArticle.appendChild(productImg);
+      productImg.src = products[i].imageUrl;
+      productImg.alt = products[i].altTxt; 
+      
+      /*Insértion de h3*/
+      const productName = document.createElement("h3");
+      productArticle.appendChild(productName);
+      productName.classList.add("productName");
+      productName.innerHTML = products[i].name;
+      
+      /*Insértion de la description p*/
+      const productDescription = document.createElement("p");
+      productArticle.appendChild(productName);
+      productDescription.classList.add("productName");
+      productDescription.innerHTML = products[i].description;
+  }
+};
+
+apiCall()
