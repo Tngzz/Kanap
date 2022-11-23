4b70b7177e9adbf7d9dd4f6c64be5ffdc8c04c59
Tanguy Dorval <dorvaltanguy@gmail.com>
2022-11-23 16:18:53 +0100

    Ajout option choix de couleur

--- front/js/product.js
+++ front/js/product.js
@@ -1,16 +1,20 @@
+/*recup de l'id dans l'url*/
 const str = window.location;
 const url = new URL(str);
 const id = url.searchParams.get("id");
+console.log(id)
 
 const productURL = url + id;
 console.log(productURL);
 
-  fetch(`http://localhost:3000/api/products/${id}`)
-    .then((reponse) => reponse.json())
-    .then((res) => productObject(res))
+/*Récup url Api + ajout de l'id*/
+fetch(`http://localhost:3000/api/products/${id}`)
+  .then((reponse) => reponse.json())
+  .then((res) => productObject(res))
 
+/*recup valeurs Api (kanap) transforme en const*/
 function productObject(kanap) {
-  console.log({kanap})
+  console.table(kanap)
   const altTxt = kanap.altTxt
   const colors = kanap.colors
   const description = kanap.description
@@ -22,16 +26,16 @@ function productObject(kanap) {
   title (name)
   neWdescription (description)
   newPrice (price)
-  /*newColors (colors)*/
+  newColors (colors)
 }
 
-  function image (imageUrl, altTxt) {
-    const image = document.createElement("img");
-    image.src = imageUrl;
-    image.alt = altTxt;
-    document.querySelector(".item__img").appendChild(image)
-
-  }
+/*affichage des produits sur la page produit*/
+function image (imageUrl, altTxt) {
+  const image = document.createElement("img");
+  image.src = imageUrl;
+  image.alt = altTxt;
+  document.querySelector(".item__img").appendChild(image)
+}
 
 function title (name) {
   document.querySelector("#title").textContent = name;
@@ -45,3 +49,53 @@ function newPrice (price) {
   document.querySelector("#price").textContent = price;
 }
 
+/*Ajout du choix des couleurs*/
+function newColors (colors) {
+   const select = document.querySelector("#colors");
+  
+  if (select != null) {
+    colors.forEach((color) => {
+      const option = document.createElement("option");
+      option.value = color;
+      option.textContent = color;
+      select.appendChild(option);
+    });
+  }
+}
+
+/*recupération option couleurs*/
+const idProduct = document.querySelector("#colors");
+console.log(idProduct)
+
+/*recupération quantity*/
+const quantityKanap = document.querySelector("#quantity");
+console.log(quantityKanap)
+
+
+/*selection du bouton dans le dom pour addEvenListenner*/
+const selectedButon = document.querySelector("#addToCart");
+console.log(addToCart);
+
+
+selectedButon.addEventListener("click", (event) => {
+  
+
+  /*récup valeur de la couleur*/
+  const choiceProduct = idProduct.value;
+ 
+  /*récup valeur de la quantity*/
+  const quantityVal = quantityKanap.value;
+  
+  let selectedProduct = {
+    idProduct: id,
+    colorProduct: choiceProduct,
+    quantity : quantityVal,
+    /*price: price,*/
+  };
+  
+  console.log(selectedProduct);
+});
+
+
+ /*local Storage*/
+ 
